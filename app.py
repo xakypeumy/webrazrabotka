2722264bdb47314a34f0ca4bcb6debf5b3ccd6c3
xakypeumy <xakypeumy4else@gmail.com>
2025-10-16 21:36:05 +0400

    добавлена функция регистрации и обновлён визуал

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import random
 import string
+import os
 
 app = Flask(__name__)
-app.secret_key = 'your-secret-key-123'
+app.secret_key = os.urandom(24)
 
 digits = '0123456789'
 uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -11,11 +12,47 @@ lowercase = 'abcdefghijklmnopqrstuvwxyz'
 punctuation = '!@#$%^&*()_+-=[]{};:,<.>/?'
 ally = digits + uppercase + lowercase + punctuation
 
+
 def load_users():
-    return [
-            {'username': 'admin', 'password': 'admin', 'role': 'admin'},
-            {'username': 'user', 'password': 'user', 'role': 'user'}
-        ]
+    users_list = []
+    try:
+        with open('users.txt', 'r', encoding='utf-8') as file:
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue
+                user_dict = {}
+                pairs = line.split(', ')
+                for pair in pairs:
+                    key, value = pair.split(': ')
+                    key = key.strip().strip("'")
+                    value = value.strip().strip("'")
+                    user_dict[key] = value
+                users_list.append(user_dict)
+
+    except FileNotFoundError:
+        print("Файл users.txt не найден.")
+    except Exception as e:
+        print(f"Ошибка при чтении файла: {e}")
+
+    return users_list
+
+
+def save_user_to_file(username, password, role):
+    user_data = f"'username': '{username}', 'password': '{password}', 'role': '{role}'"
+
+    with open('users.txt', 'a', encoding='utf-8') as file:
+        file.write(user_data + '\n')
+
+def user_exists(username):
+    try:
+        with open('users.txt', 'r', encoding='utf-8') as file:
+            for line in file:
+                if f"'username': '{username}'" in line:
+                    return True
+    except FileNotFoundError:
+        return False
+    return False
 
 def login_required(f):
     def decorated_function(*args, **kwargs):
@@ -26,6 +63,22 @@ def login_required(f):
 
     decorated_function.__name__ = f.__name__
     return decorated_function
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        role = request.form['role']
+
+        if user_exists(username):
+            return render_template('register.html',message="Пользователь с таким логином уже существует!")
+
+        save_user_to_file(username, password, role)
+
+        return render_template('register.html',message="Регистрация успешна!")
+
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
